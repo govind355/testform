4363996b65ed7bfdcd49daed0eb66ec9f19db502
govind355 <govind@cronberry.com>
2025-05-21 16:20:45 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
-// main.js
-document.addEventListener("DOMContentLoaded", () => {
-  const leads = JSON.parse(localStorage.getItem("leads")) || [];
-  document.getElementById("leadCount").textContent = leads.length;
-});
+const API_URL = "https://script.google.com/macros/s/AKfycbxB-QsDcBai4Q883FwBI2aKMVP8hp9XEktQyyoyEx8QvB8mTb6cn5oG9At5Jet4eewyLQ/exec";
+
+fetch(API_URL)
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("leadCount").textContent = data.length;
+  })
+  .catch(() => {
+    document.getElementById("leadCount").textContent = "Error loading";
+  });
